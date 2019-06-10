0732c5bc882d7a1f32dff85c408d79aa99fe542b
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-06-10 16:17:30 +0300

    get rid of css-in-js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,26 +2,17 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import './header.scss'
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `#299995`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <header>
     <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+      className="container">
+      <h1>
         <Link
           to="/"
           style={{
-            color: `white`,
-            textDecoration: `none`,
+
           }}
         >
           {siteTitle}
