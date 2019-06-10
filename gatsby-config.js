22c132c8a5cf95d81d2cd81b2cc4584f176ab9c2
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-06-10 16:16:58 +0300

    add sass plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `Oskari Peltonen`,
   },
   plugins: [
+    'gatsby-plugin-sass',
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
