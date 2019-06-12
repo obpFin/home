6cb929fce7f029edef518653b897df3999e51859
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-06-12 19:36:36 +0300

    blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const BlogPage = () => (
+  <Layout>
+    <SEO title="Blog" />
+    <h1>Blog page</h1>
+
+  </Layout>
+)
+
+export default BlogPage
