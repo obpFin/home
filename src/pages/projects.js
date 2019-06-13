d1e25f7ebe06b9fb6a6354bc582456c123887636
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-06-13 20:11:20 +0300

    projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const ProjectsPage = () => (
+  <Layout>
+    <SEO title="Projects" />
+    <h1>Projects page</h1>
+
+  </Layout>
+)
+
+export default BlogPage
