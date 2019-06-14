7853f1c2646d9d4eed46031c93dbf4267ef8298f
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-06-14 16:23:06 +0300

    image margin

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hello</h1>
     <p>This is a very unfinished version you know</p>
-    <div style={{ maxWidth: '300px'}}>
+    <div style={{ maxWidth: '300px', marginTop: '10vh'}}>
       <Image />
     </div>
   </Layout>
