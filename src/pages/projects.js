36b2c15275b9e0a8f5e12534b42ba6e1770f5b54
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-06-24 20:33:34 +0300

    query project posts

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,14 +1,45 @@
 import React from "react"
+import { graphql, useStaticQuery, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const ProjectsPage = () => (
-  <Layout>
-    <SEO title="Projects" />
-    <h1>Projects page</h1>
+const ProjectsPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  const renderPosts = () => {
+    return data.allMarkdownRemark.edges.map((post, i) => (
+      <li key={i} className="post">
+        <Link to={`/projects/${post.node.fields.slug}`}>
+          <h2 href={`/projects/${post.node.fields.slug}`}>{post.node.frontmatter.title}</h2>
+          <p>{post.node.frontmatter.date}</p>
+        </Link>
 
-  </Layout>
-)
+      </li>
+    ))
+  }
+  return (
+    <Layout>
+      <SEO title="Blog" />
+      <h1>Blog page</h1>
+      <ul>{renderPosts()}</ul>
+    </Layout>
+  )
+}
 
 export default ProjectsPage
