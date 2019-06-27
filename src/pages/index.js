8870026bfd392a67903f74f4290af374e43283c4
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-06-27 10:33:38 +0300

    fix astronaut

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <h1>Hello</h1>
     <p>This is a very unfinished version you know</p>
     <div style={{ maxWidth: '300px', marginTop: '10vh'}}>
-      <Image />
+      <Image filename="gatsby-astronaut.png"/>
     </div>
   </Layout>
 )
