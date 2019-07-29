bffb57072ac3b2d95cb022753e33c39ba742e7ca
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-07-29 14:24:45 +0300

    me page content

--- src/pages/me.js
+++ src/pages/me.js
@@ -1,15 +1,31 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import Image from "../components/image"
 import SEO from "../components/seo"
 import { ShellBlock } from "../components/shellBlock/shellBlock"
+
+const interestsIcons = ['gaming.png', 'exercising.png', 'music.png', 'photography.png']
 
 const MePage = () => (
   <Layout>
     <SEO title="Me" />
     <div className="me">
-      <ShellBlock>whois oskaripeltonen</ShellBlock>
-      <p>Me page</p>
+      <section>
+        <ShellBlock>whois oskaripeltonen</ShellBlock>
+        <p>Born and raised in the great city of Pori, Finland. Moved to Capitol area in 2017, currently living in Helsinki.</p>
+        <h2>Interests</h2>
+        <div className="me--interests">
+          {interestsIcons.map(i => {
+            return (
+              <div>
+                <Image filename={i}/>
+                <i>{i.split(".")[0]}</i>
+              </div>
+            )
+          })}
+        </div>
+      </section>
     </div>
   </Layout>
 )
