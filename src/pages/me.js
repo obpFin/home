db23d95c25576bf2215b847d3affe79b3abf006a
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-08-03 11:32:03 +0300

    refactor me page

--- src/pages/me.js
+++ src/pages/me.js
@@ -7,6 +7,12 @@ import { ShellBlock } from "../components/shellBlock/shellBlock"
 import { Linkedin, Instagram, GitHub } from 'react-feather'
 
 const interestsIcons = ['travel.png', 'gaming.png', 'exercising.png', 'music.png', 'photography.png']
+const introduction = "Born and raised in the great city of Pori, Finland. Moved to Capitol area in 2017, currently living in Helsinki."
+const social = {
+  linkedin: "https://www.linkedin.com/in/oskari-peltonen/",
+  instagram: "https://www.instagram.com/obp_fi/",
+  github: "https://github.com/obpFin"
+}
 
 const MePage = () => (
   <Layout>
@@ -14,7 +20,7 @@ const MePage = () => (
     <div className="me">
       <section>
         <ShellBlock>whois oskaripeltonen</ShellBlock>
-        <p className="mono">Born and raised in the great city of Pori, Finland. Moved to Capitol area in 2017, currently living in Helsinki.</p>
+        <p className="mono">{introduction}</p>
         <div className="row info">
           <div className="me--interests block">
             <h2>Interests</h2>
@@ -30,13 +36,13 @@ const MePage = () => (
           <div className="me--social block">
             <h2>Social</h2>
             <div className="row">
-              <a className="white" target="_blank" href="https://www.linkedin.com/in/oskari-peltonen/">
+              <a className="white" target="_blank" href={social.linkedin}>
                 <Linkedin/>
               </a>
-              <a className="white" target="_blank" href="https://www.instagram.com/obp_fi/">
+              <a className="white" target="_blank" href={social.instagram}>
                 <Instagram/>
               </a>
-              <a className="white" target="_blank" href="https://github.com/obpFin">
+              <a className="white" target="_blank" href={social.github}>
                 <GitHub/>
               </a>
             </div>
