dcc02ccfa8c3e2bc241d18e3aa2497617feb6cba
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-08-18 17:00:59 +0300

    refactor to class

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,51 +1,70 @@
 import React from "react"
-import { graphql, useStaticQuery, Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Card from "../components/card/card"
 import { ButtonGroup } from "../components/buttongroup/Buttongroup"
 
-const BlogPage = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      allMarkdownRemark {
-        edges {
-          node {
-            frontmatter {
-              title
-              date
-              tags
-              path
-            }
-            fields {
-              slug
-            }
-          }
-        }
-      }
+const blogFilters = {
+  work: 'work',
+  misc: 'misc'
+}
+
+class BlogPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      activeFilter: blogFilters.work
     }
-  `)
-  const isBlogPost = post => post.node.frontmatter.path.includes("blog")
-  const renderPosts = () => {
+  }
+
+  isBlogPost = post => post.node.frontmatter.path.includes("blog")
+
+  changeFilter = name => this.setState({ activeFilter: blogFilters[name] })
+    
+  renderPosts = () => {
+    const data = this.props.data
     return data.allMarkdownRemark.edges.map(
       (data, i) =>
-        isBlogPost(data) && (
+        this.isBlogPost(data) && (
           <Link key={i} to={`/blog/${data.node.fields.slug}`}>
             <Card data={data} />
           </Link>
         )
     )
   }
-  return (
-    <Layout>
-      <SEO title="Blog" />
-      <div className="blog">
-        <ButtonGroup initActive="work" />
-        <div className="projects grid">{renderPosts()}</div>
-      </div>
-    </Layout>
-  )
+  render() {
+    return (
+      <Layout>
+        <SEO title="Blog" />
+        <div className="blog">
+          <ButtonGroup active={this.state.activeFilter} changeFilter={this.changeFilter}/>
+          <div className="projects grid">{this.renderPosts()}</div>
+        </div>
+      </Layout>
+    )
+  }
 }
+
+export const pageQuery = graphql`
+query {
+  allMarkdownRemark {
+    edges {
+      node {
+        frontmatter {
+          title
+          date
+          tags
+          path
+        }
+        fields {
+          slug
+        }
+      }
+    }
+  }
+}
+`
 
 export default BlogPage
