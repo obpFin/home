4f9d67bb6da40a8fa109a085c9da2ee30bc067fd
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-08-18 17:00:38 +0300

    refactor to stateless

--- src/components/buttongroup/buttongroup.js
+++ src/components/buttongroup/buttongroup.js
@@ -1,37 +1,26 @@
 import React from "react"
 
-export class ButtonGroup extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { active: this.props.initActive }
-  }
-
-  changeCategory = (name) => this.setState({active: name})
-    
-  render() {
-    return (
-      <div className="buttongroup">
-        <input
-          id="work"
-          key='work'
-          type="radio"
-          value="work"
-          name="market"
-          onChange={() => this.changeCategory("work")}
-          checked={this.state.active == "work"}
-        />
-        <label htmlFor="work">Work</label>
-        <input
-          id="misc"
-          key='misc'
-          type="radio"
-          value="misc"
-          name="market"
-          onChange={() => this.changeCategory("misc")}
-          checked={this.state.active == "misc"}
-        />
-        <label htmlFor="misc">Misc</label>
-      </div>
-    )
-  }
-}
+export const ButtonGroup = props => (
+  <div className="buttongroup">
+    <input
+      id="work"
+      key="work"
+      type="radio"
+      value="work"
+      name="market"
+      onChange={() => props.changeFilter("work")}
+      checked={props.active == "work"}
+    />
+    <label htmlFor="work">Work</label>
+    <input
+      id="misc"
+      key="misc"
+      type="radio"
+      value="misc"
+      name="market"
+      onChange={() => props.changeFilter("misc")}
+      checked={props.active == "misc"}
+    />
+    <label htmlFor="misc">Misc</label>
+  </div>
+)
